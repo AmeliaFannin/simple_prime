47ee8cf0c04f17c9c5fccacdd3099f2ed13ed183
Amelia Fannin <ameliafannin@gmail.com>
2014-07-22 12:56:32 -0400

    added comments

--- simple_prime.go
+++ simple_prime.go
@@ -1,5 +1,6 @@
 package simple_prime
 
+// sends possible primes, less than max, to primeCheck function
 func PrimeGenerator(max int) chan int {
 	out := make(chan int)
 	primes := []int{2}
@@ -8,15 +9,21 @@ func PrimeGenerator(max int) chan int {
 		out <- 2
 		for i := 3; i <= max; i += 2 {
 			if primeCheck(primes, i) {
+
+				// puts confirmed primes into the out channel
 				out <- i
+
+				// adds confirmed primes to the array,
 				primes = append(primes, i)
 			}
 		}
 		close(out)
 	}()
+	// returns channel of confirmed primes
 	return out
 }
 
+// checks possible prime for divisibility by confirmed primes
 func primeCheck(primes []int, candidate int) bool {
 	for _, known_prime := range primes {
 		if candidate%known_prime == 0 {
